eddfe901d1c9f03305cca6f7eb621cd0ad6cd33f
Pbt <68289322+PbtServers@users.noreply.github.com>
2022-03-17 20:22:22 +0100

    Delete Vistas.java

--- src/main/java/com/pbtservers/pbtclient/Vistas.java
+++
@@ -1,9 +0,0 @@
-package com.pbtservers.pbtclient;
-
-public class Vistas implements VistasApi {
-	 @Override
-	 public void appendPanoramas(Map<Identifier, Panorama> set) {
-	  // All your panorama magic goes here!
-	  set.put(new Identifier("mod", "panorama_1"), Panorama.DEFAULT);
-	 }
-}
