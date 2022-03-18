7a879413d89995572aae8cff53ebafe96ca35fd1
Pbt <Pbt@PbtServers.com>
2022-03-18 12:53:04 +0100

    Primer Mixin!

---
+++ src/main/java/com/pbtservers/pbtclient/mixin/TitleMixin.java
@@ -0,0 +1,9 @@
+package com.pbtservers.pbtclient.mixin;
+
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(TitleScreen.class)
+public class TitleMixin {
+	
+}
